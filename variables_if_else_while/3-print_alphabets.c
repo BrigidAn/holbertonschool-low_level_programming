f83e3f1410b5af500f07d23d7679510642ded112
root <ochimerema@gmail.com>
2026-01-12 09:15:21 +0000

    Task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the alphabet in lowercase followed by uppercase,
+ * using only putchar and three calls to putchar.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}

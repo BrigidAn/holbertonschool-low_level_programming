93a87545f489c793dd72d423f361c9253e51aef4
BrigidAn <ochimerema@gmail.com>
2026-01-14 08:55:26 +0000

    Task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,13 +7,13 @@
  */
 void print_alphabet(void)
 {
-    char letter = 'a';
+	char letter = 'a';
 
-    while (letter <= 'z')  /* Loop from 'a' to 'z' */
-    {
-        _putchar(letter);  /* Print each letter */
-        letter++;  /* Move to the next letter */
-    }
+	while (letter <= 'z')  /* Loop from 'a' to 'z' */
+	{
+		_putchar(letter);  /* Print each letter */
+		letter++;  /* Move to the next letter */
+	}
 
-    _putchar('\n');  /* Print the newline character */
+	_putchar('\n');  /* Print the newline character */
 }

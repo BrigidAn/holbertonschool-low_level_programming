a9605aa0fa37c307a01cb2025440bd98791836fe
BrigidAn <ochimerema@gmail.com>
2026-01-15 06:50:36 +0000

    Task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,34 +1,39 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table, starting with 0.
+ * times_table - prints the 9 times table, starting with 0
  */
 void times_table(void)
 {
-    int i, j, result;
+	int i, j, result;
 
-    for (i = 0; i < 10; i++)  /* Loop through rows */
-    {
-        for (j = 0; j < 10; j++)  /* Loop through columns */
-        {
-            result = i * j;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
 
-            if (j == 0)
-            {
-                _putchar('0' + result);  /* Print the first number */
-            }
-            else
-            {
-                _putchar(',');  /* Print comma separator */
-                _putchar(' ');  /* Print space between numbers */
+			if (j == 0)
+			{
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
 
-                if (result < 10)  /* Add extra space for single-digit numbers */
-                {
-                    _putchar(' ');
-                }
-                _putchar('0' + result);  /* Print result */
-            }
-        }
-        _putchar('\n');  /* Move to the next line after each row */
-    }
+				if (result < 10)
+				{
+					_putchar(' ');
+					_putchar(result + '0');
+				}
+				else
+				{
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
 }

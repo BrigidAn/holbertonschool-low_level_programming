0dd15681f261ba6165a04f7ec2e3208ea90a0e3c
BrigidAn <ochimerema@gmail.com>
2026-01-15 13:28:26 +0000

    Task 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: pointer to the string
+ *
+ * Return: the integer value of the string, or 0 if no number found
+ */
+int _atoi(char *s)
+{
+	int i;
+	int sign;
+	int result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+
+	/* Skip non-digit characters and count signs */
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] == '+')
+			sign *= 1;
+		i++;
+	}
+
+	/* Convert the numeric characters */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (result * sign);
+}

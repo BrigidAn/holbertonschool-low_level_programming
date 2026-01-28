4022b88a8c45a966b39b9666f21fbd2d5ed00492
root <ochimerema@gmail.com>
2026-01-28 08:50:38 +0000

    Task 1

---
+++ more_malloc_free/1-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - test string_nconcat
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    char *concat;
+
+    concat = string_nconcat("Best ", "School !!!", 6);
+    printf("%s\n", concat); /* Expected: "Best School" */
+    free(concat);
+
+    return (0);
+}

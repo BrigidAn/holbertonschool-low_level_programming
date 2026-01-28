386f1a59c6b3bf5d29a9844e9fb1face755fc994
root <ochimerema@gmail.com>
2026-01-28 08:57:21 +0000

    Task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * string_nconcat - concatenates s1 and first n bytes of s2
@@ -15,24 +14,34 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
     char *new_str;
     unsigned int len1 = 0, len2 = 0, i, j;
 
+    /* Calculate length of s1 manually */
     if (s1 != NULL)
-        len1 = strlen(s1);
-    if (s2 != NULL)
-        len2 = strlen(s2);
+        while (s1[len1])
+            len1++;
 
+    /* Calculate length of s2 manually */
+    if (s2 != NULL)
+        while (s2[len2])
+            len2++;
+
+    /* If n is bigger than s2, use full s2 */
     if (n > len2)
         n = len2;
 
+    /* Allocate memory for new string */
     new_str = malloc(len1 + n + 1);
     if (new_str == NULL)
         return (NULL);
 
+    /* Copy s1 */
     for (i = 0; i < len1; i++)
         new_str[i] = s1[i];
 
+    /* Copy first n bytes of s2 */
     for (j = 0; j < n; j++)
         new_str[i + j] = s2[j];
 
+    /* Null terminate */
     new_str[i + j] = '\0';
 
     return (new_str);

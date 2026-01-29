3f1d9889aa112e7849ba7634656eff9334ee0072
BrigidAn <ochimerema@gmail.com>
2026-01-29 13:25:34 +0000

    task 1

---
+++ variadic_functions/1-main.c
@@ -0,0 +1,16 @@
+#include "variadic_functions.h"
+#include <stddef.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_numbers(", ", 4, 0, 98, -1024, 402);
+	print_numbers(NULL, 3, 1, 2, 3);
+	print_numbers(" - ", 0);
+
+	return (0);
+}

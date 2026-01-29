2fc1173ef0a8b173475ea41bd8f050030c157f07
BrigidAn <ochimerema@gmail.com>
2026-01-29 09:18:47 +0000

    Task2

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -7,5 +7,6 @@ void print_name(char *name, void (*f)(char *));
 void printnameasis(char *name);
 void printnameuppercase(char *name);
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
